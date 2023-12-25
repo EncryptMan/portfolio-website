8017d2ebf550752d93d4a63ceeed352026a4fd09
Muhammad Umar <dibble.garages_0g@icloud.com>
2023-12-25 14:53:50 +0500

    Fix apostrophe errors

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
         <div className="rounded-full overflow-hidden border-4 border-white shadow-3xl shadow-black relative w-32 h-32">
         <Image src={'/profile.jpeg'} alt="Muhammad Umar's profile picture" fill className="object-cover" />
         </div>
-        <h1 className='text-5xl max-w-2xl mt-7'>Hello, I'm <span className="font-bold">Muhammad Umar</span>. I'm a high school student and a software developer with 4 years of experience.</h1>
+        <h1 className='text-5xl max-w-2xl mt-7'>{"Hello, I'm"} <span className="font-bold">Muhammad Umar</span>{". I'm a high school student and a software developer with 4 years of experience."}</h1>
       </section>
       <div className="flex items-center justify-center my-20">
       <div className="w-1 h-20 bg-slate-500/30" />
       </div>
       <section id="about" className="flex flex-col items-center justify-center text-center mt-20">
         <h1 className='text-4xl font-bold'>About Me</h1>
-        <p className="text-2xl mt-5 max-w-2xl">I'm a high school student from Pakistan. My interests are computer science and technology. As a hobby I like to do software development. My first passion was game development. Later, I switched to full-stack development.</p>
+        <p className="text-2xl mt-5 max-w-2xl">{"I'm a high school student from Pakistan. My interests are computer science and technology. As a hobby I like to do software development. My first passion was game development. Later, I switched to full-stack development."}</p>
       </section>
       <ProjectsSection />
       <SkillsSection />
